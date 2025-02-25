8b04f723554aa354d35421c00da137b23e86067c
bettina.ssilva <bettina.ssilva@TIT0533381W10-1>
2025-02-25 19:25:29 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, /n World!");
+
+Console.Write("Bettina /n");
+Console.Write("Santos ");
+
+string firstName = "Bettina ";
+string lastName = "Silva";
+string surname = "Santos ";
+
+int idade = 18;
+
+Console.WriteLine("Meu nome completo é " + firstName + surname + lastName + ",a minha idade é " + idade);
+
+Console.WriteLine("Escreva o seu nome: ");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Olá {nome}, boa noite !!!");
